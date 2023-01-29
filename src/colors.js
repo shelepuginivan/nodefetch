050002a14f47b2d5d733a429d9d6cc37c328fec1
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-29 19:10:22 +0300

    ascii escape colors

---
+++ src/colors.js
@@ -0,0 +1,5 @@
+export const GREEN = '\u001b[32m'
+export const BOLD = '\u001b[1m'
+export const RESET = '\u001b[0m'
+
+export const BG_COLORS = ["\u001b[40m", "\u001b[41m", "\u001b[42m", "\u001b[43m", "\u001b[44m", "\u001b[45m", "\u001b[46m", "\u001b[47m"]
