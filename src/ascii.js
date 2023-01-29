eb1b78695e3d57621dce5e6bf6d9dd54d709b331
shelepuginivan <shelepuginivanm@gmail.com>
2023-01-29 19:10:08 +0300

    ascii Node.js logo

---
+++ src/ascii.js
@@ -0,0 +1,19 @@
+import {GREEN, RESET} from './colors.js'
+
+export const nodeAsciiLogo = [
+	` ${GREEN}            j@@@@@@@@&${RESET}`,
+	` ${GREEN}      .;j@@@@@*    *@@@@@&;.${RESET}`,
+	` ${GREEN}  j@@@@@@               *@@@@@@&${RESET}`,
+	` ${GREEN}/@@@                         *@@@${RESET}`,
+	` ${GREEN}@@       @@@@     @@@@@@@@&    @@${RESET}`,
+	` ${GREEN}@@       @@@@   J@@      ^@@@  @@${RESET}`,
+	` ${GREEN}@@       @@@@   @@@&           @@${RESET}`,
+	` ${GREEN}@@       @@@@     @@@@@@@@@    @@${RESET}`,
+	` ${GREEN}@@       @@@@             @@@  @@${RESET}`,
+	` ${GREEN}@@       @@@@   @@@&     J@@^  @@${RESET}`,
+	` ${GREEN}@@       @@@@     @@@@@@@@@    @@${RESET}`,
+	` ${GREEN}\\@@@   ./@@@@                @@@/${RESET}`,
+	` ${GREEN} \\@@@@@@@@@/          .:@@@@@@/${RESET}`,
+	` ${GREEN}     *@@@^  /&       @@@@@/${RESET}`,
+	` ${GREEN}            \\@@@@@@@@@/${RESET}`,
+]
